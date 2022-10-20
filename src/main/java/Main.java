ff2c327c963bb3bf35c1f502ad63e4116983a943
page404info <page404.info@gmail.com>
2022-10-20 12:40:30 +0300

    feat: add swing start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
+import helper.PropertyConfigReader;
 import lombok.extern.log4j.Log4j2;
 import module_1_init.Starter;
+import module_1_init.StarterSwing;
 import module_2_crawler.CrawlerWeb;
 import module_3_parser.PageParser;
 import module_4_report.ReportCreator;
+
+import javax.swing.*;
 
 @Log4j2
 public class Main {
     public static void main(String[] args) {
         log.debug(new Exception().getStackTrace()[0].getMethodName());
 
-        new Starter().start();
+        new StarterSwing().start();
         new CrawlerWeb().search();
         new PageParser().parse();
         new ReportCreator().create();
+        JOptionPane.showMessageDialog(null, "Finish " + PropertyConfigReader.getInstance().getSrcUrl()
+        + "\nopen " + PropertyConfigReader.getInstance().getSrcDir());
     }
 }
